284f2bd13370df2c6936dc865dcfd67623084282
Bobby Sporman <bobby.sporman@yahoo.com>
2020-08-01 19:01:26 -0400

    Updates

---
+++ index.js
@@ -0,0 +1,49 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const path = require("path");
+
+inquirer.prompt([
+{
+    type: 'input',
+    name: 'repoName',
+    message: 'What is name of this application? (This will aslo the main header of the README)',
+ 
+},
+//asks user for git hub user name 
+{
+    type: 'input',
+    name: 'gitHubName',
+    message: 'What is your username on Github?',
+ 
+},
+// asks for a description of the project 
+{
+    type: 'input',
+    message: 'Write a description of this application',
+    name: 'description',
+ 
+},
+// How do you install your app? 
+{
+    type: 'input',
+    message: 'How does a user install your application? Provide instuctions here',
+    name: 'installApp',
+ 
+},
+// How does a user use your application? 
+{
+    type: 'input',
+    message: 'How does a user use your application? Provide instuctions here',
+    name: 'appInstructions',
+ 
+},
+// How would a user of this application report an issue, or make a contribution to it? 
+{
+    type: 'input',
+    message: 'Would you like you users to be able to report issues or contribute to this project? add instructions here.',
+    name: 'appIssuesAndCont',
+}
+
+])
